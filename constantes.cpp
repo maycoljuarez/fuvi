a8b97e684cd3ac0ac87043253f598960899f423b
Electronica 2022 <electronica2022@fuvi.net>
2022-10-04 09:40:10 -0600

    se agrego denuevo el archivo constantes.cpp

---
+++ constantes.cpp
@@ -0,0 +1,16 @@
+/*
+Nombre del archivo: sumanumeros.cpp
+autor: danilo noguera
+lugar: itv
+intrucciones: ejemplo de expresiones matematicas
+*/
+
+#include<iostream>
+
+using namespace std;
+
+int main(){
+	
+	return 0;
+	
+}
